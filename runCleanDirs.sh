f3d4b791623bbb16ef53ccd1e8e5756c8997b258
Charlie <ncsucharlie@gmail.com>
2018-04-22 09:42:22 -0400

    Add append option to tee commands in wrapper bash script

--- runCleanDirs.sh
+++ runCleanDirs.sh
@@ -4,7 +4,7 @@ LOGFILE="output.log"
 
 date | tee $LOGFILE
 bash /ssd/plexMedia/setPermissions.sh
-echo "" | tee $LOGFILE
-echo "Cleaning up target folders in /ssd/plexMedia" | tee $LOGFILE
-echo "============================================" | tee $LOGFILE
-node cleanDirs.js --base_scan_dir "/ssd/plexMedia" | tee $LOGFILE
+echo "" | tee --append $LOGFILE
+echo "Cleaning up target folders in /ssd/plexMedia" | tee --append $LOGFILE
+echo "============================================" | tee --append $LOGFILE
+node cleanDirs.js --base_scan_dir "/ssd/plexMedia" | tee --append $LOGFILE
