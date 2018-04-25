e8192e766001c4c9fe9ecebd3134cf33c84a0a81
Charlie <ncsucharlie@gmail.com>
2018-04-25 16:36:36 -0400

    Comment out need to run setPermissions in plex base dir

--- runCleanDirs.sh
+++ runCleanDirs.sh
@@ -2,9 +2,10 @@ cd /home/charlie/github/clean-torrent-dirs
 
 LOGFILE="output.log"
 
-date | tee $LOGFILE
-bash /ssd/plexMedia/setPermissions.sh
+date | --append tee $LOGFILE
+#bash /ssd/plexMedia/setPermissions.sh
 echo "" | tee --append $LOGFILE
 echo "Cleaning up target folders in /ssd/plexMedia" | tee --append $LOGFILE
 echo "============================================" | tee --append $LOGFILE
 node cleanDirs.js --base_scan_dir "/ssd/plexMedia" | tee --append $LOGFILE
+echo "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*" | tee --append $LOGFILE
