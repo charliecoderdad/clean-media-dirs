04a06eaa8d8ffcec2e2fde4b025936cd16f8582d
Charlie <ncsucharlie@gmail.com>
2018-04-25 20:07:06 -0400

    Add clearer separator string between runs

--- runCleanDirs.sh
+++ runCleanDirs.sh
@@ -1,11 +1,11 @@
 cd /home/charlie/github/clean-torrent-dirs
-
 LOGFILE="output.log"
 
 date | --append tee $LOGFILE
-#bash /ssd/plexMedia/setPermissions.sh
 echo "" | tee --append $LOGFILE
 echo "Cleaning up target folders in /ssd/plexMedia" | tee --append $LOGFILE
-echo "============================================" | tee --append $LOGFILE
+echo "--------------------------------------------" | tee --append $LOGFILE
 node cleanDirs.js --base_scan_dir "/ssd/plexMedia" | tee --append $LOGFILE
-echo "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*" | tee --append $LOGFILE
+echo "********************************************************************************" | tee --append $LOGFILE
+echo "********************************************************************************" | tee --append $LOGFILE
+echo "********************************************************************************" | tee --append $LOGFILE
